9c8b87c7b33b63e74b17d52eddc246e31d16d868
Anna-andrade <annaandrade@id.uff.br>
2022-09-01 16:58:26 -0300

    Initial Commit

---
+++ Poc02Notifications/Poc02NotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poc02NotificationsApp.swift
+//  Poc02Notifications
+//
+//  Created by Anna Carolina Costa Andrade on 01/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Poc02NotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
